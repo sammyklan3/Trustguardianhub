5d93c287bb214cf6562c60db6d9e488e7c563128
sammyklan3 <sammyklane7@gmail.com>
2024-03-30 12:37:59 +0300

    axios config update

--- src/api/axiosInstance.jsx
+++ src/api/axiosInstance.jsx
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.VITE_ENVIRONMENT === "production" ? `${import.meta.VITE_PRODUCTION_BACKEND_BASE_URL}/api/` : `${import.meta.env.VITE_LOCAL_BACKEND_BASE_URL}/api/`; // Use the 'process' module to access environment variables
+const BASE_URL = import.meta.env.VITE_ENVIRONMENT === "production"
+  ? `${import.meta.env.VITE_PRODUCTION_BACKEND_BASE_URL}/api/`
+  : `${import.meta.env.VITE_LOCAL_BACKEND_BASE_URL}/api/`;
+
+
+console.log("BASE_URL: ", BASE_URL);
 
 export const axiosInstance = axios.create(
   {
